784b5458d31fdb9a7b71e5d3150ff3be54c7d9d7
Cody Brown <cody6lefever@gmail.com>
2020-04-28 15:45:04 -0700

    userdisplay component made

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import UserDisplay from './UserDisplay/UserDisplay';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <UserDisplay
+    username="Cody"
+    profilePic="this is a pic" 
+    profileLink="this is a link" 
+    repoLink="this is also a link" 
+    followerCount="20"
+    followingCount="30" />;
 }
   
---
+++ src/components/App/UserDisplay/UserDisplay.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const UserDisplay = ({ username, profilePic, profileLink, repoLink, followerCount, followingCount }) => (
+  <div>
+    {username}
+    {profilePic}
+    {profileLink}
+    {repoLink}
+    {followerCount}
+    {followingCount}
+  </div>
+);
+
+UserDisplay.propTypes = {
+  username: PropTypes.string.isRequired,
+  profilePic: PropTypes.string.isRequired,
+  profileLink: PropTypes.string.isRequired,
+  repoLink: PropTypes.string.isRequired,
+  followerCount: PropTypes.number.isRequired,
+  followingCount: PropTypes.number.isRequired
+}
+
+export default UserDisplay;
